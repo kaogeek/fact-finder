2e72142651882f23bd2d1f1b91e83a27da328446
chaintng <chaintng@gmail.com>
2021-08-18 01:36:08 +0700

    add cloud function for twitter bot

--- backend/twitter-bot/index.js
+++ backend/twitter-bot/index.js
@@ -4,10 +4,21 @@ const functions = require("firebase-functions");
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-exports.twitterBot = functions
-    .region("asia-southeast1")
-    .https.onRequest((request, response) => {
-      // Put some logic here
-      functions.logger.info("Hello logs!", {structuredData: true});
-      response.send("Hello from Firebase!");
-    });
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.scoutTwitter = functions.pubsub.schedule("0 7 * * *").onRun((context) => {
+    const dateTimeNow = new Date();
+    const writeResult = admin.firestore().collection('twitterBotConfig').doc("LAST_UPDATE");
+    writeResult.set({ date: dateTimeNow });
+    functions.logger.info(`Last Update: ${dateTimeNow.toString()} (ID: ${writeResult.id})`);
+    response.send(`Last Update: ${dateTimeNow.toString()}`);
+});
+
+exports.scountTwitterLastUpdate = functions
+  .https.onRequest(async (request, response) => {
+    const configRef = await admin.firestore().collection('twitterBotConfig');
+    const lastUpdateRef = await configRef.doc("LAST_UPDATE").get();
+    functions.logger.info(`Last Update: ${lastUpdateRef.data().date.toDate().toString()}`, {structuredData: true});
+    response.send(`Last Update: ${lastUpdateRef.data().date.toDate().toString()}`);
+});
