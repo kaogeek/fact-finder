5b9cb30c9a77526fd9ef0375dfe02f075877378f
chaintng <chaintng@gmail.com>
2021-08-19 00:57:59 +0700

    retrieve mentioned tweet

---
+++ backend/twitter-bot/README.md
@@ -0,0 +1,5 @@
+## Main Tasks
+1. [DONE] Fetch tweets and save to Firestore.
+2. [WORKING] Define necessary tweet details data.
+3. Add necessary tweet details into Firestore. Each type of report tweet have different way to get details.
+4. Filter only important tweets.
--- backend/twitter-bot/index.js
+++ backend/twitter-bot/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const TwitterApi = require("./TwitterApi")
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,18 +8,67 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.scoutTwitter = functions.pubsub.schedule("0 7 * * *").onRun((context) => {
-    const dateTimeNow = new Date();
-    const writeResult = admin.firestore().collection('twitterBotConfig').doc("LAST_UPDATE");
-    writeResult.set({ date: dateTimeNow });
-    functions.logger.info(`Last Update: ${dateTimeNow.toString()} (ID: ${writeResult.id})`);
-    response.send(`Last Update: ${dateTimeNow.toString()}`);
-});
+/**
+ * Logic to decide whether to save tweet or not.
+ * @param {Tweet} tweet Single Tweet response from Tweet API
+ */
+function shouldSaveTweet(tweet) {
+  const publicMetrics = tweet.public_metrics;
+  const haveAttachments = !!tweet.attachments;
+  const highEngagement = publicMetrics.retweet_count > 1000 || publicMetrics.reply_count > 10 || publicMetrics.like_count > 500 || publicMetrics.quote_count > 20;
+  if (haveAttachments && highEngagement) {
+    return tweetNotExistedInDb(tweet);
+  }
+}
 
-exports.scountTwitterLastUpdate = functions
-  .https.onRequest(async (request, response) => {
-    const configRef = await admin.firestore().collection('twitterBotConfig');
-    const lastUpdateRef = await configRef.doc("LAST_UPDATE").get();
-    functions.logger.info(`Last Update: ${lastUpdateRef.data().date.toDate().toString()}`, {structuredData: true});
-    response.send(`Last Update: ${lastUpdateRef.data().date.toDate().toString()}`);
+/**
+ * Check whether current tweet already have in db yet?
+ * @param {*} tweet 
+ */
+function tweetNotExistedInDb(tweet) {
+  return true; // TODO search firestore by tweet id.
+}
+
+/**
+ * Save record tweet to database
+ * @param {Tweet} tweet Single Tweet response from Tweet API
+ */
+async function saveTweetToDb(tweet) {
+  // TODO Save Media files
+  // TODO Add necessary data
+  // TODO Correct date timestamp
+  const recordData = {
+    referenceType: "TWIITER",
+    referneceUrl: `https://twitter.com/user/status/${tweet.id}`,
+    timestamp: new Date(tweet.created_at),
+  }
+  const writeResult = await admin.firestore().collection('records').add(recordData);
+  return writeResult;
+}
+
+/**
+ * Set Last Update Tweet Id to Firestore Twitter Bot Configuration
+ * for continue query tweet later
+ * @param {Tweet} tweet Single Tweet response from Tweet API
+ */
+ function setLastUpdate(tweet) {
+  const lastUpdateTweetId = admin.firestore().collection('twitterBotConfig').doc("LAST_UPDATE_TWEET_ID");
+  lastUpdateTweetId.set({ id: tweet.id });
+}
+
+// In production, uncomment this to let schedule run.
+// exports.scoutTwitter = functions.pubsub.schedule("0 7 * * *").onRun((context) => {
+exports.scoutTwitter = functions.https.onRequest(async (request, response) => {
+  const lastUpdateTweetId = await admin.firestore().collection('twitterBotConfig').doc("LAST_UPDATE_TWEET_ID").get();
+    const tweets = await TwitterApi.getMentionedTweet(lastUpdateTweetId)
+    tweets.map(async (item) => {
+      recordTweet = await TwitterApi.getRecordTweetDetails(item)
+      if (recordTweet && shouldSaveTweet(recordTweet)) {
+        await saveTweetToDb(recordTweet)
+      }
+      setLastUpdate(item)
+    })
+
+    const afterUpdateTweetId = await admin.firestore().collection('twitterBotConfig').doc("LAST_UPDATE_TWEET_ID").get();
+    response.send(`Last Update Tweet Id: ${afterUpdateTweetId.data() && afterUpdateTweetId.data().id}`);
 });
