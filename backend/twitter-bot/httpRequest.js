97279d0405e4e394daf4ffc6b9dd4bdee2d97e63
Chainarong Tangsurakit <chaintng@gmail.com>
2021-08-19 10:12:55 +0700

    add cloud function for twitter bot (#16)
    * add cloud function for twitter bot
    
    * retrieve mentioned tweet
    
    * Add region
    
    Co-authored-by: Natthaphong Ruengpanyawut <tee4cute@gmail.com>

---
+++ backend/twitter-bot/httpRequest.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+const { error } = require("firebase-functions/lib/logger");
+const { URL, URLSearchParams } = require('url');
+
+exports.httpRequest = async (urlString, params, requestOptions) => {
+
+    const url = new URL(urlString);
+    if (requestOptions.method == "GET") {
+        url.search = new URLSearchParams(params).toString();
+    }
+    const response = await fetch(url, requestOptions)
+    if (response.status !== 200) {
+        error("HTTP Status:", response.status, ":", response.statusText);
+        return false;
+    }
+    const responseJson = await response.json();
+    if (!responseJson.data) {
+        error(responseJson.title, ":", responseJson.errors[0].message);
+        return false;
+    }
+    return responseJson.data;
+}
--- backend/twitter-bot/index.js
+++ backend/twitter-bot/index.js
@@ -1,13 +1,76 @@
 const functions = require("firebase-functions");
+const TwitterApi = require("./TwitterApi")
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-exports.twitterBot = functions
-    .region("asia-southeast1")
-    .https.onRequest((request, response) => {
-      // Put some logic here
-      functions.logger.info("Hello logs!", {structuredData: true});
-      response.send("Hello from Firebase!");
-    });
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+/**
+ * Logic to decide whether to save tweet or not.
+ * @param {Tweet} tweet Single Tweet response from Tweet API
+ */
+function shouldSaveTweet(tweet) {
+  const publicMetrics = tweet.public_metrics;
+  const haveAttachments = !!tweet.attachments;
+  const highEngagement = publicMetrics.retweet_count > 1000 || publicMetrics.reply_count > 10 || publicMetrics.like_count > 500 || publicMetrics.quote_count > 20;
+  if (haveAttachments && highEngagement) {
+    return tweetNotExistedInDb(tweet);
+  }
+}
+
+/**
+ * Check whether current tweet already have in db yet?
+ * @param {*} tweet 
+ */
+function tweetNotExistedInDb(tweet) {
+  return true; // TODO search firestore by tweet id.
+}
+
+/**
+ * Save record tweet to database
+ * @param {Tweet} tweet Single Tweet response from Tweet API
+ */
+async function saveTweetToDb(tweet) {
+  // TODO Save Media files
+  // TODO Add necessary data
+  // TODO Correct date timestamp
+  const recordData = {
+    referenceType: "TWIITER",
+    referneceUrl: `https://twitter.com/user/status/${tweet.id}`,
+    timestamp: new Date(tweet.created_at),
+  }
+  const writeResult = await admin.firestore().collection('records').add(recordData);
+  return writeResult;
+}
+
+/**
+ * Set Last Update Tweet Id to Firestore Twitter Bot Configuration
+ * for continue query tweet later
+ * @param {Tweet} tweet Single Tweet response from Tweet API
+ */
+ function setLastUpdate(tweet) {
+  const lastUpdateTweetId = admin.firestore().collection('twitterBotConfig').doc("LAST_UPDATE_TWEET_ID");
+  lastUpdateTweetId.set({ id: tweet.id });
+}
+
+// In production, uncomment this to let schedule run.
+// exports.scoutTwitter = functions.pubsub.schedule("0 7 * * *").onRun((context) => {
+exports.scoutTwitter = functions
+  .region("asia-southeast1")
+  .https.onRequest(async (request, response) => {
+    const lastUpdateTweetId = await admin.firestore().collection('twitterBotConfig').doc("LAST_UPDATE_TWEET_ID").get();
+    const tweets = await TwitterApi.getMentionedTweet(lastUpdateTweetId)
+    tweets.map(async (item) => {
+      recordTweet = await TwitterApi.getRecordTweetDetails(item)
+      if (recordTweet && shouldSaveTweet(recordTweet)) {
+        await saveTweetToDb(recordTweet)
+      }
+      setLastUpdate(item)
+    })
+
+    const afterUpdateTweetId = await admin.firestore().collection('twitterBotConfig').doc("LAST_UPDATE_TWEET_ID").get();
+    response.send(`Last Update Tweet Id: ${afterUpdateTweetId.data() && afterUpdateTweetId.data().id}`);
+});
