691167cf6b50bbb5b8ab782c6d788d12ac784f30
tino.t <tino@vtno.me>
2021-08-19 21:33:24 +0300

    Fix lint issue

--- backend/twitter-bot/TwitterApi.js
+++ backend/twitter-bot/TwitterApi.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const { httpRequest } = require("./httpRequest");
 const { Headers } = require("node-fetch");
 
