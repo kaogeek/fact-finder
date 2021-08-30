f493af62c2de607836a5f49aac42179cabdb7389
Natthaphong Ruengpanyawut <natthaphongr@absolute.co.th>
2021-08-30 13:42:25 +0700

    VIDEO files are now supported by `gdrive-bot`
    VIDEO file support implementation w/ `eslint` enabled.

--- backend/gdrive-bot/functions/.eslintrc.js
+++ backend/gdrive-bot/functions/.eslintrc.js
@@ -27,5 +27,8 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
+    "require-jsdoc": 0,
+    "max-len": 0,
+    "@typescript-eslint/no-var-requires": 0,
   },
 };
